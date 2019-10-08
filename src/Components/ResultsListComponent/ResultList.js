90dd83d96ae33aead54e28ad3224f7f7c520d5a6
Juan Ortiz <juanortiz@Juans-MacBook-Pro.local>
2019-10-08 12:21:16 -0500

    Added styles and icons for the location search feature

--- src/Components/ResultsListComponent/ResultList.js
+++ src/Components/ResultsListComponent/ResultList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ResultsList.css';
+import StarRating from '../StarRating';
 
 export default class ResultList extends React.Component {
     constructor(props) {
@@ -12,16 +13,17 @@ export default class ResultList extends React.Component {
                 {data.map(x => 
                     <div key={x.id} className ="resultsListItemDiv">
                         <h3>{x.name}</h3>
-                        <p>Rating: {x.rating}</p>
+                        <StarRating rating={x.rating}/>
+
                         <p>Distance from marker: {x.distanceToUser.toFixed(3)} Km</p>
                         <div className="addressDiv">
-                            <h4>Location: </h4>
+                            <h4><span className="fa fa-map-marker checked"/> Address: </h4>
                             <p>Street: {x.address.street}</p>
                             <p>State: {x.address.state}</p>
                             <p>City: {x.address.city}</p>
                         </div>
                         <div className="contactDiv">
-                            <h4>Contact: </h4>
+                            <h4><span className="fa fa-user checked"/> Contact: </h4>
                             <p>Email: {x.contact.email}</p>
                             <p>Phone: {x.contact.phone}</p>
                             <p>Site: {x.contact.site}</p>
