9a00f8dbf1e3b08cea79880c288769be10bef40f
Juan Ortiz <juanortiz@Juans-MacBook-Pro.local>
2019-10-07 20:34:24 -0500

    Updates

---
+++ precache-manifest.b0826cdb2b411632ad209067572b994f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6095ba41a33f0ee2f12d268c317af654",
+    "url": "/melp-client-app/index.html"
+  },
+  {
+    "revision": "b4c14b133e333640dec5",
+    "url": "/melp-client-app/static/css/main.f27a2113.chunk.css"
+  },
+  {
+    "revision": "0637633df6cf02569c21",
+    "url": "/melp-client-app/static/js/2.b015f8ff.chunk.js"
+  },
+  {
+    "revision": "b4c14b133e333640dec5",
+    "url": "/melp-client-app/static/js/main.20324e0e.chunk.js"
+  },
+  {
+    "revision": "f8fea679ab368bfdd0af",
+    "url": "/melp-client-app/static/js/runtime-main.cfffe2ac.js"
+  }
+]);
